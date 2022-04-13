9ed0aa2088108e429575f344d301cdd3d1c6be6d
raffaeler <raffaeler@vevy.com>
2022-04-13 09:20:55 +0200

    Additional test assembly

---
+++ src/LibraryTest/DerivedLevel1Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpeedyGenerators;
+
+namespace LibraryTest
+{
+    internal partial class DerivedLevel1Class : BaseClass
+    {
+        //[MakeProperty("Name")]
+        //private string _name = string.Empty;
+
+
+        //[MakeProperty("Hello", false, false)]
+        //private int _a;
+
+
+        //public void Ciao()
+        //{
+        //    Name = "Ciao";
+        //    Hello = 10;
+        //}
+
+    }
+}
---
+++ src/LibraryTest/ShortDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SpeedyGenerators;
+
+namespace LibraryTest
+{
+    internal partial class ShortDemo
+    {
+        [MakeProperty("Name")]
+        private string? _name;
+    }
+}
